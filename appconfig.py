a936f9a053a04bbfd4ae8773af745b1d09e94788
Justyna Gredecka <justynagredecka@outlook.com>
2019-07-20 19:49:04 +0100

    Update script

--- appconfig.py
+++ appconfig.py
@@ -6,5 +6,5 @@ MAX_CONTENT_LENGTH = 6 * 1024 * 1024
 # session expiry
 PERMANENT_SESSION_LIFETIME = timedelta(minutes=6)
 # secret key
-SECRET_KEY = '\xea\x99DkZN\xc1\x9f\x01n/\xa5\x91\x9f\xfc\x02-\x03\n\x8d\x0f\xd5\xcas'
+SECRET_KEY = os.urandom(24)
 DEBUG = True
